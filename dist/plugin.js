3fcfc09d1e6b684d8eab318f676ad288b6ac143a
Massimo Melina <a@rejetto.com>
2022-08-03 23:45:04 +0200

    Initial commit

---
+++ dist/plugin.js
@@ -0,0 +1,5 @@
+exports.version = 1
+exports.description = "This is a dumb theme, meant to be just an example for creators"
+exports.apiRequired = 1
+exports.repo = "rejetto/theme-example"
+exports.frontend_css = 'style.css'
