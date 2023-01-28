446ae6a05b317e7e54fa0e354b41d3bf9028082c
Massimo Melina <a@rejetto.com>
2023-01-28 17:02:14 +0100

    comply with current version of HFS (0.29.1)

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1
+exports.version = 1.1
 exports.description = "This is a dumb theme, meant to be just an example for creators"
 exports.apiRequired = 1
 exports.repo = "rejetto/theme-example"
